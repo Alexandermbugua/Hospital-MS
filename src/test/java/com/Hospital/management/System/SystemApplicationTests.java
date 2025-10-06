69b49e8b584828f388584dd0fce4a58a9d856e07
Alex <119705389+Alexandermbugua@users.noreply.github.com>
2025-10-06 15:21:32 +0300

    Initial project setup

---
+++ src/test/java/com/Hospital/management/System/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hospital.management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
